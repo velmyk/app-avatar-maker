3864ddf78e4ea6ad38b30073a5665e89b601011c
velmyk <velmyk@gmail.com>
2016-05-17 14:36:01 +0300

    feature(build) added hashes to static files

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,15 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const webpack = require('webpack'),
 	autoprefixer = require('autoprefixer')
-	ExtractTextPlugin = require('extract-text-webpack-plugin');
+	ExtractTextPlugin = require('extract-text-webpack-plugin'),
+	HtmlWebpackPlugin = require('html-webpack-plugin'),
+	rimraf = require('rimraf');
 
+function addHash(template, hash) {
+	return NODE_ENV === 'production'
+		? template.replace(/\.[^.]+$/, `.[${hash}]$&`)
+		: `${template}?hash=${hash}`;
+} 
 
 module.exports = {
 	context: __dirname + '/src',
@@ -14,7 +21,7 @@ module.exports = {
     output: {
         path: __dirname + '/target',
         pablicpath: '/',
-        filename: '[name].js',
+        filename: addHash('[name].js', 'chunkhash'),
     },
     watch: NODE_ENV == 'development',
     watchOptions: {
@@ -31,11 +38,21 @@ module.exports = {
 		extensions: ['', '.js'] 
 	},
 	plugins: [
+		{
+			apply: (compiler) => {
+				rimraf.sync(compiler.options.output.path)
+			}
+		},
 		new webpack.NoErrorsPlugin(),
 		new webpack.DefinePlugin({
 			NODE_ENV: JSON.stringify(NODE_ENV)
 		}),
-		new ExtractTextPlugin('[name].css', { allChanks: true })
+		new ExtractTextPlugin(addHash('[name].css', 'contenthash'), { allChanks: true }),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './index.html',
+			chunks: ['app']
+		})
 	],
 	module: {
 		loaders: [
@@ -62,7 +79,7 @@ module.exports = {
             {
             	test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
             	include: /\/node_modules\//,
-            	loader: 'file?name=[1].[ext]&regExp=node_modules/(.*)'
+            	loader: addHash('file?name=[1].[ext]&regExp=node_modules/(.*)', 'hash:6')
             },
             // {
             // 	test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
@@ -71,7 +88,7 @@ module.exports = {
             {
             	test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
             	exclude: /\/node_modules\//,
-            	loader: 'file?name=[path][name].[ext]'
+            	loader: addHash('file?name=[path][name].[ext]', 'hash:6')
             }
 		],
 		noParse: /angular\/angular.js/
