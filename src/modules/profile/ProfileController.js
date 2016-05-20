0e2bff2d9788e82ef8db32ddc87e1c5d148a1199
velmyk <velmyk@gmail.com>
2016-05-20 15:49:03 +0300

    feature(avatar options panel) added buttons for gender choose

--- src/modules/profile/ProfileController.js
+++ src/modules/profile/ProfileController.js
@@ -8,6 +8,7 @@ export default class ProfileController {
 		FabricService.Image.fromURL('../../src/images/face_default.png', oImg => {
 			oImg.setFlipX(true);
 			oImg.selectable = false;
+			oImg.selectable = false;
 			oImg.top = 100;
 			oImg.left = 100;
   			canvas.add(oImg);
@@ -15,8 +16,17 @@ export default class ProfileController {
 
 		FabricService.Image.fromURL('../../src/images/hair-mail.png', oImg => {
 			oImg.setFlipX(true);
+			oImg.selectable = false;
 			oImg.top = 60;
 			oImg.left = 110;
+  			canvas.add(oImg);
+		});
+
+		FabricService.Image.fromURL('../../src/images/hair-femail.png', oImg => {
+			oImg.setFlipX(true);
+			oImg.selectable = false;
+			oImg.top = 50;
+			oImg.left = 105;
   			canvas.add(oImg);
 		});
 
--- src/modules/profile/profile.module.js
+++ src/modules/profile/profile.module.js
@@ -3,8 +3,6 @@ import uirouter from 'angular-ui-router';
 
 import routes from './profileRoutes';
 
-import './profile.scss';
-
 export default angular.module('app.profile', [uirouter])
 	.config(routes)
 	.name;
