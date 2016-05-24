5260f89030fad77d496b635143fa70a8d91371a9
velmyk <velmyk@gmail.com>
2016-05-24 15:09:02 +0300

    fix(profile page) changed the way to show elements

--- src/modules/profile/ProfileController.js
+++ src/modules/profile/ProfileController.js
@@ -1,16 +1,17 @@
 import AVATAR_ELEMENTS from './AVATAR_ELEMENTS';
 
 export default class ProfileController {
-	constructor($window, FabricService, $timeout) {
+	constructor(FabricService, $timeout, $q) {
 		'ngInject';
 
 		this.FabricService = FabricService;
 		this.$timeout = $timeout;
+		this.$q = $q;
 
 		this.canvas = new FabricService.Canvas('background');
 
 		this.avatar = {
-			face: AVATAR_ELEMENTS.face.default,
+			face: null,
 			hair: null,
 			eyes: null,
 			eyebrows: null,
@@ -18,40 +19,51 @@ export default class ProfileController {
 			mouth: null
 		};
 					
-		this.refreshAvatar();
+		this.initialize();
+    }
+
+    initialize() {
+    	this.addFace('default');
+    	this.drawEyes();
     }
 
 	addImageElement(canvas, element) {
-		this.FabricService.Image.fromURL(element.imageUrl, oImg => {
-			oImg.setFlipX(true);
-			oImg.selectable = false;
-			oImg.top = element.top;
-			oImg.left = element.left;
-			canvas.add(oImg);
+		return this.$q((resolve, reject) => {
+			this.FabricService.Image.fromURL(element.imageUrl, oImg => {
+				oImg.setFlipX(true);
+				oImg.selectable = false;
+				oImg.top = element.top;
+				oImg.left = element.left;
+
+				resolve(oImg);
+			});
 		});
 	}
 
-	drawElement(canvas, element) {
+	addElement(canvas, element) {
 		let path = new fabric.Path(element.path);
 		path.set(element.options);
-		canvas.add(path);
+		return path;
 	}
 
 	refreshAvatar() {
-		let element;
-
 		this.clearAvatar();
-		Object.keys(this.avatar).forEach(key => {
-			element = this.avatar[key];
-			if (element) {
-				element.path
-					? this.drawElement(this.canvas, element)
-					: this.addImageElement(this.canvas, element);
-			}
-		});
+		this.displayElements();
 	}
 
-	addEyes() {
+	displayElements() {
+		Object.keys(this.avatar).forEach(key => this.avatar[key] ? this.canvas.add(this.avatar[key]) : '')
+	}
+
+	addFace(faceType) {
+		this.addImageElement(this.canvas, AVATAR_ELEMENTS.face[faceType])
+			.then(face => {
+				this.avatar.face = face;
+				this.refreshAvatar();
+			});
+	}
+
+	drawEyes() {
 		const
 			leftEye = new this.FabricService.Circle(AVATAR_ELEMENTS.eyes.left),
 			rightEye = new this.FabricService.Circle(AVATAR_ELEMENTS.eyes.right);
@@ -59,26 +71,28 @@ export default class ProfileController {
 		this.avatar.eyes = new this.FabricService.Group([leftEye, rightEye], {
 			selectable: false,
 		});
-		this.canvas.add(this.avatar.eyes);
 	}
 
 	addHair(hairType) {
-		this.avatar.hair = AVATAR_ELEMENTS.hair[hairType];
-		this.refreshAvatar();
+		this.addImageElement(this.canvas, AVATAR_ELEMENTS.hair[hairType])
+			.then(hair => {
+				this.avatar.hair = hair;
+				this.refreshAvatar();
+			});
 	}
 
 	addEyebrows(eyebrowsType) {
-		this.avatar.eyebrows = AVATAR_ELEMENTS.eyebrows[eyebrowsType];
+		this.avatar.eyebrows = this.addElement(this.canvas, AVATAR_ELEMENTS.eyebrows[eyebrowsType]);
 		this.refreshAvatar();
 	}
 
 	addNose(noseType) {
-		this.avatar.nose = AVATAR_ELEMENTS.nose[noseType];
+		this.avatar.nose = this.addElement(this.canvas, AVATAR_ELEMENTS.nose[noseType]);
 		this.refreshAvatar();
 	}
 
 	addMouth(mouthType) {
-		this.avatar.mouth = AVATAR_ELEMENTS.mouth[mouthType];
+		this.avatar.mouth = this.addElement(this.canvas, AVATAR_ELEMENTS.mouth[mouthType]);
 		this.refreshAvatar();
 	}
 
@@ -93,13 +107,13 @@ export default class ProfileController {
 	}
 
 	animateEyes() {
-		this.addEyes();
+		this.avatar.eyes.set('opacity', 0);
 		this.$timeout(() => {
-			this.avatar.eyes.animate('opacity', 0, {
+			this.avatar.eyes.animate('opacity', 1, {
 				duration: 200,
 				onChange: this.canvas.renderAll.bind(this.canvas),
 				onComplete: this.animateEyes.bind(this)
-			})
+			});
 		}, 2000);
 	}
 }
